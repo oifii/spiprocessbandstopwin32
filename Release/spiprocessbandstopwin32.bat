3228a57b859007f3a76bec11a95819c15dff6d8f
Stephane Poirier <stephane.poirier@oifii.org>
2016-12-12 08:35:54 -0500

    first commit

---
+++ Release/spiprocessbandstopwin32.bat
@@ -0,0 +1,3 @@
+rem start spiprocessbandstopwin32.exe "E-MU ASIO" 0 1 "E-MU ASIO" 2 3 100.0 -1 1 1 900 1000 220 1 0 0 24 255 200 0 "mywindowclass" "mywindowtitle" "" ""
+rem start spiprocessbandstopwin32.exe "E-MU ASIO" 4 5 "E-MU ASIO" 2 3 100.0 -1 1 1 900 1000 220 1 0 0 24 255 200 0 "mywindowclass" "mywindowtitle" "" ""
+start spiprocessbandstopwin32.exe "E-MU ASIO" 6 7  "E-MU ASIO" 2 3 500.0 -1 1 1 900 1000 220 1 0 0 24 255 200 0 "mywindowclass" "mywindowtitle" "" ""
